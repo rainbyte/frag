63cfd9208cbd1257d6672f5ed3cf6f53793a7056
Alvaro Fernando García <alvarofernandogarcia@gmail.com>
2017-05-07 18:07:09 -0300

    R&C: Cleanup Game module. Remove redundant code (e.g. unneeded case .. of occurrences)

--- src/Game.hs
+++ src/Game.hs
@@ -15,17 +15,13 @@ import Visibility (aiVisTest)
 
 game :: BSPMap -> [ILKey -> Object] -> SF GameInput [ObsObjState]
 game bspmap objs
-  = (loop
+  = loop
        (game'
-          (case (listToILA $ objs) of
-               x -> x)
+          (listToILA objs)
           bspmap
           >>> arr (\ oos -> (oos, oos)))
        >>>
-       arr
-         (\ oos ->
-            case (map ooObsObjState (elemsIL oos)) of
-                y -> y))
+       arr (map ooObsObjState . elemsIL)
 
 game' ::
       IL Object -> BSPMap -> SF (GameInput, IL ObjOutput) (IL ObjOutput)
@@ -35,35 +31,31 @@ game' objs bspmap
 
 route ::
       BSPMap -> (GameInput, IL ObjOutput) -> IL sf -> IL (ObjInput, sf)
-route bspmap (gi, oos) objs = mapIL routeAux objs
+route bspmap (gi, oos) = mapIL routeAux
   where routeAux (k, obj)
-          = case (find (\ (x, _, _) -> k == x) states) of
+          = case find (\ (x, _, _) -> k == x) states of
                 Just (_, _, z) -> (z, obj)
                 Nothing -> (ObjInput{oiHit = noEvent, oiMessage = noEvent,
                                      oiCollisionPos = (0, 0, 0), oiCollision = dummy,
                                      oiOnLand = False, oiVisibleObjs = noEvent, oiGameInput = gi},
                             obj)
         messages
-          = concat $ map eventToList $ elemsIL $ fmap ooSendMessage oos
+          = concatMap eventToList $ elemsIL $ fmap ooSendMessage oos
         states
-          = (clips bspmap gi (assocsIL $ fmap ooObsObjState oos)
-               (assocsIL $ fmap ooObsObjState oos))
+          = clips bspmap gi (assocsIL $ fmap ooObsObjState oos)
+               (assocsIL $ fmap ooObsObjState oos)
               messages
         dummy = initCamera (80::Int, 611::Int, 60::Int) (80::Int, 611::Int, 59::Int) (0::Int, 1::Int, 0::Int)
 
 killOrSpawn :: (a, IL ObjOutput) -> Event (IL Object -> IL Object)
 killOrSpawn (_, oos)
-  = case (foldl (mergeBy (.)) noEvent es) of
-        y -> y
+  = foldl (mergeBy (.)) noEvent es
   where
         es :: [Event (IL Object -> IL Object)]
         es
-          = case
-              ([mergeBy (.) (ooKillReq oo `tag` (deleteIL k))
+          =    [mergeBy (.) (ooKillReq oo `tag` deleteIL k)
                   (fmap (foldl (.) id . map insertILA_) (ooSpawnReq oo))
-                | (k, oo) <- assocsIL oos])
-              of
-                x -> x
+                | (k, oo) <- assocsIL oos]
 
 clips ::
       BSPMap ->
@@ -73,20 +65,21 @@ clips ::
               [(ILKey, (ILKey, Message))] -> [(ILKey, ObsObjState, ObjInput)]
 clips _ _ [] _ _ = []
 clips mp gi ((k, oos) : kooss) ooses msgs
-  = case (clip mp gi oos ooses k msgs) of
-        x -> (k, oos, x) : (clips mp gi kooss ooses msgs)
+  = let x = clip mp gi oos ooses k msgs
+        xs = clips mp gi kooss ooses msgs
+    in (k, oos, x) : xs
 
 clip :: BSPMap -> GameInput -> ObsObjState -> [(ILKey, ObsObjState)] ->
        ILKey -> [(ILKey, (ILKey, Message))] -> ObjInput
-clip mp gi cam@(OOSCamera{newCam = cam1, oldCam = cam2}) ooses k msgs
+clip mp gi cam@OOSCamera{newCam = cam1, oldCam = cam2} ooses k msgs
   = let (camera, grounded) = clipCamera mp cam1 cam2 in
       ObjInput{oiHit = listToEvent $ findCollidingObjects ooses (k, cam),
                oiMessage = findMessages k msgs, oiCollisionPos = (0, 0, 0),
                oiCollision = camera, oiOnLand = grounded, oiVisibleObjs = noEvent,
                oiGameInput = gi}
 clip mp gi
-  acube@(OOSAICube{oosNewCubePos = pos1, oosOldCubePos = pos2,
-                   oosCubeSize = sz, health = h, target = t})
+  acube@OOSAICube{oosNewCubePos = pos1, oosOldCubePos = pos2,
+                  oosCubeSize = sz, health = h, target = t}
   ooses k msgs
   = let (clippedPos, grounded) = clipObject mp pos1 pos2 sz in
       ObjInput{oiHit =
@@ -95,14 +88,15 @@ clip mp gi
                oiMessage = findMessages k msgs, oiCollisionPos = clippedPos,
                oiOnLand = grounded,
                oiVisibleObjs =
-                 (case (h == 100 && t == (0, 0, 0)) of
-                      True -> (listToEvent $ findVisibleTargets mp ooses (k, acube) 600)
-                      _ -> (listToEvent $ findVisibleTargets mp ooses (k, acube) 1800)),
+                 let value = if h == 100 && t == (0, 0, 0)
+                             then 600
+                             else 1800
+                 in listToEvent $ findVisibleTargets mp ooses (k, acube) value,
                oiGameInput = gi}
 clip mp gi
   OOSRay{rayStart = pos1, rayEnd = pos2, rayUC = pos3, clipped = ff}
   _ k msgs
-  | ff == False =
+  | not ff =
     let (_, hasCol) = clipRay mp pos3 pos1 (0, 0, 0) in
       ObjInput{oiHit = noEvent, oiMessage = findMessages k msgs,
                oiCollision = dummy, oiCollisionPos = fix hasCol pos2 pos3 pos1,
@@ -112,18 +106,18 @@ clip mp gi
              oiCollision = dummy, oiCollisionPos = pos2, oiOnLand = True,
              oiVisibleObjs = noEvent, oiGameInput = gi}
   where fix clpped p2 p3 p1
-          | clpped == True = tryAccurate mp 4 p2 p3 p1
+          | clpped    = tryAccurate mp 4 p2 p3 p1
           | otherwise = pos3
         dummy = initCamera (80::Int, 611, 60) (80::Int, 611, 59) (0, 1, 0)
 clip mp gi
-  projectile@(OOSProjectile{projectileNewPos = pos3,
-                            projectileOldPos = pos1})
+  projectile@OOSProjectile{projectileNewPos = pos3,
+                           projectileOldPos = pos1}
   ooses k _
   = let (clippedPos, hasCol) = clipRay mp pos3 pos1 (0, 0, 0) in
       ObjInput{oiHit =
                  listToEvent $ findCollidingObjects ooses (k, projectile),
                oiMessage = noEvent,
-               oiCollision = (initCamera (80::Int, 611, 60) (80::Int, 611, 59) (0, 1, 0)),
+               oiCollision = initCamera (80::Int, 611, 60) (80::Int, 611, 59) (0, 1, 0),
                oiCollisionPos = clippedPos, oiOnLand = hasCol,
                oiVisibleObjs = noEvent, oiGameInput = gi}
 
@@ -138,8 +132,8 @@ eventToList ev
 
 tryAccurate :: BSPMap -> Int -> Vec3 -> Vec3 -> Vec3 -> Vec3
 tryAccurate mp n dv1 dv2 vec
-  | n == 0 = (middle dv1 dv2 vec)
-  | snd (clipRay mp (middle dv1 dv2 vec) vec (0, 0, 0)) == True =
+  | n == 0 = middle dv1 dv2 vec
+  | snd (clipRay mp (middle dv1 dv2 vec) vec (0, 0, 0)) =
     tryAccurate mp (n - 1) dv1 (middle dv1 dv2 vec) vec
   | otherwise = tryAccurate mp (n - 1) (middle dv1 dv2 vec) dv2 vec
   where middle v1 v2 _ = vectorAdd v1 (vectorMult (vectorSub v2 v1) 0.5)
@@ -161,10 +155,10 @@ findCollidingObjects ooses (k, obj)
 checkCollision :: ObsObjState -> ObsObjState -> Bool
 checkCollision obj1 obj2
   | isCamera obj2 && isProjectile obj1 =
-    pointBox (cpos (oldCam (obj2))) (projectileOldPos obj1)
+    pointBox (cpos (oldCam obj2)) (projectileOldPos obj1)
       (20, 50, 20)
   | isCamera obj1 && isProjectile obj2 =
-    pointBox (cpos (oldCam (obj1))) (projectileOldPos obj2)
+    pointBox (cpos (oldCam obj1)) (projectileOldPos obj2)
       (20, 50, 20)
   | isAICube obj1 && isRay obj2 =
     let (x, y, z) = oosOldCubePos obj1
